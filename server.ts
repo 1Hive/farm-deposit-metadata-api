b35704a7055f4b24fda34e511c0b59327cc56d7f
Hernando Guzmán <hernando.guzman93@gmail.com>
2021-07-07 18:37:28 -0300

    Initialize Express server

---
+++ server.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello world!'))
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`))
